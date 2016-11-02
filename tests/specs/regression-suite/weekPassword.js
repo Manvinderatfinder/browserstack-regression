f6661a311aac4cf4e0e8b86f76d52f423a3ba8d8
Mans <manvinder@hiveempire.com>
2016-11-02 15:59:45 +1100

    Added new tests

---
+++ tests/specs/regression-suite/weekPassword.js
@@ -0,0 +1,22 @@
+var assert = require('assert');
+describe('Test for an existing user signup', function() {
+ it('should display error message for an existing user', function () {
+   browser
+            .url('https://www.finder.com.au/credit-score')
+            .setValue('*[name="emailAddress"]','tester1@hiveempire.com')
+                    .pause(500);
+        browser
+            .setValue('*[name="password"]','test123')
+            .click('.credit-check-sign-up__submit')
+            .pause(1000);
+         browser
+     var result = browser.getText('.form__group.is_invalid > .form__error');
+      assert(result, 'Your password is not secure enough');
+    });
+});
+
+
+
+
+
+    
